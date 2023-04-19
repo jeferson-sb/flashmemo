390281662a362d4c48b50b0dae68c37b0a674a30
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-04-18 22:41:56 -0300

    feat: add jbuilder for questions

---
+++ api/app/views/questions/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.array! @questions do |question|
+  json.id question.id
+  json.title question.title
+  json.created_at question.created_at
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  scope '/api' do
+    # resources :answers, only: %[index show]
+    # resources :questions, only: %[index show]
+    get "answers/:id", to: "answers#show"
+    get "questions/:id", to: "questions#show"
+    get "questions", to: "questions#index"
+  end
 end
