ef94501276f16785f31a878c3122f147d6d576e7
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-04-19 19:51:31 -0300

    feat: add rspec

---
+++ api/spec/models/question_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  subject { Question.create(title: 'What is RSPec?') }
+
+  it 'has title attribute' do
+    question = Question.create(title: 'What is Ruby?')
+    expect(question.title).to eq('What is Ruby?')
+  end 
+
+  context 'when option created' do
+    it 'has options attributes' do
+      question = Question.create(title: 'question1')
+      firstOption = Option.create(text: 'answer 1', correct: false, question: Question.first)
+      secondOption = Option.create(text: 'answer 2', correct: true, question: Question.first)
+
+      expect(question.options).to eq(Option.all)
+    end 
+  end 
+end
