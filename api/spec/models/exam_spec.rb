aa3fbfec827516a43b592918b9f0241dacdb0697
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-05-09 20:58:30 -0300

    fix: exam model validations spec

---
+++ api/spec/models/exam_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Exam, type: :model do
+  subject { create(:exam) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:difficulty) }
+    it { is_expected.to validate_presence_of(:version) }
+    it { is_expected.to validate_uniqueness_of(:title) }
+  end
+
+  describe 'relations' do
+    it { is_expected.to have_many(:questions) }
+  end 
+end
