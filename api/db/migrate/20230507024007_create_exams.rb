323bf20a6d6263e87a167877820343e812870cb2
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-05-09 20:48:39 -0300

    feat: add exams migrations

---
+++ api/db/migrate/20230507024007_create_exams.rb
@@ -0,0 +1,11 @@
+class CreateExams < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exams do |t|
+      t.string :title, unique: true
+      t.string :difficulty
+      t.integer :version
+
+      t.timestamps
+    end
+  end
+end
---
+++ api/db/migrate/20230507195301_add_exam_relation_on_questions.rb
@@ -0,0 +1,5 @@
+class AddExamRelationOnQuestions < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :questions, :exam, foreign_key: true
+  end
+end
