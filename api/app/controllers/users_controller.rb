4ef2d8132ae57fa1500bff9605effbfce21b4ee8
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-09-27 20:43:05 -0300

    refact: get_answers method to use get_dates

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  MONTH = { :january => 1, :june => 6, :july => 7, :december => 12 }
+
   def create
     @user = User.new(create_params.slice(:username, :email))
 
@@ -30,28 +32,31 @@ class UsersController < ApplicationController
     params.permit(:username, :email)
   end
 
-  def get_semester_dates(today)
-    if today.month >= 7
-      [Date.new(today.year, 1).beginning_of_month, Date.new(today.year, 6).end_of_month]
+  def get_semester_dates(date)
+    if date.month >= MONTH[:july]
+      [Date.new(date.year, MONTH[:january]).beginning_of_month, Date.new(date.year, MONTH[:june]).end_of_month]
     else
-      [Date.new(today.year - 1, 7).beginning_of_month, Date.new(today.year - 1, 12).end_of_month]
+      [Date.new(1.year.ago.year, MONTH[:july]).beginning_of_month, Date.new(1.year.ago.year, MONTH[:december]).end_of_month]
     end
   end
 
-  def get_answers(user_id, period)
-    today = Time.now
-
-    start_date, end_date =
-      case period
+  def get_dates(period)
+    current_date = Time.now
+  
+    case period
       when 'monthly'
-        [today - 30.days, nil]
+        [30.days.ago, nil]
       when 'yearly'
-        [today - 1.year, nil]
+        [1.year.ago, nil]
       when 'semester'
-        get_semester_dates(today)
+        get_semester_dates(current_date)
       else
         [nil, nil]
       end
+  end
+
+  def get_answers(user_id, period)
+    start_date, end_date = get_dates(period)
 
     Answer.where(user_id:, created_at: start_date..end_date)
   end
