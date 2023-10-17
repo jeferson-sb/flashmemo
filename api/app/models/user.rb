3870f3286889b4fe8076d646508f6eab377d753c
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2023-10-17 20:08:39 -0300

    feat: add password to User

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # has_secure_password
+  has_secure_password
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :answer
   has_many :exams, through: :answer
