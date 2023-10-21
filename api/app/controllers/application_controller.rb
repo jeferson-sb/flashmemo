4170975f346d5e1a621c8faa6452de186a607a5e
Jeferson S. Brito <30840709+jeferson-sb@users.noreply.github.com>
2023-10-21 17:59:19 -0300

    refact: extract token from Authorization header
    Co-authored-by: Miguel Marcondes Filho <47646621+miguelmarcondesf@users.noreply.github.com>

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -4,15 +4,22 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_request
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
+    token = extract_token_from_header
+
     begin
-      @decoded = JsonWebToken.decode(header)
+      @decoded = JsonWebToken.decode(token)
       @user = User.find(@decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
     end
+  end
+  
+  private
+  
+  def extract_token_from_header
+    header = request.headers['Authorization']
+    header&.split(' ')&.last
   end
 end
