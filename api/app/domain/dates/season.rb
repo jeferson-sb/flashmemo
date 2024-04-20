a4c93bacb47bb5a946c73a1e45ade251f72a9f59
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2024-04-20 16:04:23 -0300

    refact: season quarters

--- api/app/domain/dates/season.rb
+++ api/app/domain/dates/season.rb
@@ -4,37 +4,34 @@ module Dates
   class Season
     class << self
       QUARTERS = { 1 => 0.25, 2 => 0.50, 3 => 0.75 }.freeze
+      SEASONS = {
+        winter: { north: 1, south: 3 },
+        spring: { north: 2, south: 4 },
+        summer: { north: 3, south: 1 },
+        autumn: { north: 4, south: 2 }
+      }.freeze
 
       def from(date, hemisphere = :north)
+        validate_hemisphere(hemisphere)
+
         day_of_year = date.yday
         length = Date.leap?(date.year) ? 366 : 365
+        quarter = calculate_quarter(day_of_year, length)
 
-        case hemisphere
-        when :north
-          case day_of_year
-          when 1..(length * QUARTERS[1]).floor
-            :winter
-          when (length * QUARTERS[1]).floor + 1..(length * QUARTERS[2]).floor
-            :spring
-          when (length * QUARTERS[2]).floor + 1..(length * QUARTERS[3]).floor
-            :summer
-          else
-            :autumn
-          end
-        when :south
-          case day_of_year
-          when 1..(length * QUARTERS[1]).floor
-            :summer
-          when (length * QUARTERS[1]).floor + 1..(length * QUARTERS[2]).floor
-            :autumn
-          when (length * QUARTERS[2]).floor + 1..(length * QUARTERS[3]).floor
-            :winter
-          else
-            :spring
-          end
-        else
-          raise ArgumentError, 'Invalid hemisphere'
+        season = SEASONS.find { |_, v| v[hemisphere] == quarter }
+        season ? season.first : raise("Season not found for hemisphere: #{hemisphere}, quarter: #{quarter}")
+      end
+
+      private
+
+      def calculate_quarter(day_of_year, length)
+        QUARTERS.each do |quarter, multiplier|
+          return quarter if day_of_year <= (length * multiplier).floor
         end
+      end
+
+      def validate_hemisphere(hemisphere)
+        raise ArgumentError, 'Invalid hemisphere' unless %i[north south].include?(hemisphere)
       end
     end
   end
