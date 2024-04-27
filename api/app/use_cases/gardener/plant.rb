cc2e31b544517f1dac0abc9805c22a38d4a32a75
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2024-04-27 16:05:38 -0300

    refact: split Plant method into smaller functions

--- api/app/use_cases/gardener/plant.rb
+++ api/app/use_cases/gardener/plant.rb
@@ -4,10 +4,24 @@ module Gardener
   class Plant
     class << self
       def perform(garden, name)
-        raise 'Not enough seeds' if garden.seeds <= 0
+        raise 'Not enough seeds' if insufficient_seeds?(garden)
 
+        create_tree(garden, name)
+        consume_seed(garden)
+      end
+      
+      private
+
+      def insufficient_seeds?(garden)
+        garden.seeds <= 0
+      end
+
+      def create_tree(garden, name)
         tree = garden.trees.new(name:, phase: :seed)
         tree.validate!
+      end
+
+      def consume_seed(garden)
         garden.seeds -= 1
         garden.save!
       end
