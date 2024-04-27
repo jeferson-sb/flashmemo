2153ad16572c16dcf2b979747a7147ab4638f40a
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2024-04-27 16:01:34 -0300

    refact: split nurture method into smaller functions

--- api/app/use_cases/gardener/nurture.rb
+++ api/app/use_cases/gardener/nurture.rb
@@ -4,16 +4,41 @@ module Gardener
   class Nurture
     class << self
       def perform(tree, garden, nutrients)
-        raise 'Not enough nutrients' if garden.nutrients.negative? || garden.nutrients < nutrients
-        raise 'Nutrients should be greater than 1' if nutrients <= 0
-        raise 'Tree health should be lower than 100' if tree.health == 100
-        raise 'Cannot use more nutrients than 100' if tree.health + nutrients > 100
+        raise 'Not enough nutrients' if insufficient_nutrients?(garden, nutrients)
+        raise 'Nutrients should be greater than 1' if negative_nutrients?(nutrients)
+        raise 'Tree health should be lower than 100' if tree_health_full?(tree)
+        raise 'Cannot use more nutrients than 100' if exceeded_nutrients_limit?(tree, nutrients)
 
+        update_tree_health(tree, nutrients)
+        update_garden_nutrients(garden)
+      end
+
+      private
+
+      def update_tree_health(tree, nutrients)
         tree.health += nutrients
         tree.save!
+      end
 
+      def update_garden_nutrients(garden)
         garden.nutrients -= 1
         garden.save!
+      end
+
+      def insufficient_nutrients?(garden, nutrients)
+        garden.nutrients.negative? || garden.nutrients < nutrients
+      end
+
+      def negative_nutrients?(nutrients)
+        nutrients <= 0
+      end
+
+      def tree_health_full?(tree)
+        tree.health == 100
+      end
+
+      def exceeded_nutrients_limit?(tree, nutrients)
+        tree.health + nutrients > 100
       end
     end
   end
