90cdfef63edccdf73ae74ea9fa26d5c3b021a4ff
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2024-08-10 17:27:34 -0300

    refact: user progress

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def progress
-    @answers = get_answers(params[:user_id], params[:time])
+    @answers = Answers::InRange.perform(params[:user_id], params[:time])
 
     if !@answers.empty?
       average, exams = Users::Progress.perform(@answers)
@@ -33,35 +33,5 @@ class UsersController < ApplicationController
 
   def create_params
     params.permit(:name, :username, :email, :password)
-  end
-
-  def get_semester_dates(date)
-    if date.month >= MONTH[:july]
-      [Date.new(date.year, MONTH[:january]).beginning_of_month, Date.new(date.year, MONTH[:june]).end_of_month]
-    else
-      [Date.new(1.year.ago.year, MONTH[:july]).beginning_of_month,
-       Date.new(1.year.ago.year, MONTH[:december]).end_of_month]
-    end
-  end
-
-  def get_dates(period)
-    current_date = Time.now
-
-    case period
-    when 'monthly'
-      [30.days.ago, nil]
-    when 'yearly'
-      [1.year.ago, nil]
-    when 'semester'
-      get_semester_dates(current_date)
-    else
-      [nil, nil]
-    end
-  end
-
-  def get_answers(user_id, period)
-    start_date, end_date = get_dates(period)
-
-    Answer.where(user_id:, created_at: start_date..end_date)
   end
 end
---
+++ api/app/use_cases/answers/in_range.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Answers
+  class InRange
+    class << self
+      def perform(user_id, period)
+        start_date, end_date = get_dates(period)
+
+        Answer.where(user_id:, created_at: start_date..end_date)
+      end
+
+      private
+
+      def get_semester_dates(date)
+        if date.month >= MONTH[:july]
+          [Date.new(date.year, MONTH[:january]).beginning_of_month, Date.new(date.year, MONTH[:june]).end_of_month]
+        else
+          [Date.new(1.year.ago.year, MONTH[:july]).beginning_of_month,
+           Date.new(1.year.ago.year, MONTH[:december]).end_of_month]
+        end
+      end
+
+      def get_dates(period)
+        current_date = Time.now
+
+        case period
+        when 'monthly'
+          [30.days.ago, nil]
+        when 'yearly'
+          [1.year.ago, nil]
+        when 'semester'
+          get_semester_dates(current_date)
+        else
+          [nil, nil]
+        end
+      end
+    end
+  end
+end
